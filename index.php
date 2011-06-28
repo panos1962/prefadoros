0845becc8d9736ff07374b0c7fb9a55756305724
panos1962 <panos1962@localhost>
2011-06-28 14:20:43 +0300

    modifications

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Διαδικτυακή πρέφα" />
+		<meta name="keywords" content="<?php print 'πρέφα, χαρτοπαίγνιο, ' .
+			'πρεφαδόρος, ατού, αγορά, μπάζα, μπάζες, σόλο, σολαρία, κάσα, ' .
+			'καπίκι, καπίκια'; ?>" />
+		<title>Πρεφαδόρος</title>
+	</head>
+	<body>
+		Ok!
+	</body>
+</html>
