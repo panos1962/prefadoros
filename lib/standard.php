54011ce9d2805b4cc2fddba6a99840dcdfa2049f
panos1962 <panos1962@localhost>
2011-06-29 08:58:50 +0300

    modifications

--- lib/standard.php
+++ lib/standard.php
@@ -1,3 +1,4 @@
+
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -9,13 +10,6 @@
 			'πρεφαδόρος, ατού, αγορά, μπάζα, μπάζες, σόλο, σολαρία, ' .
 			'κάσα, καπίκι, καπίκια'; ?>" />
 		<title>Πρεφαδόρος</title>
-		<script type="text/javascript">
-			//<![CDATA[
-			globals = {};
-			globals.server = 'http://127.0.0.1/prefadoros/';
-			globals.timeDif = <?php print time(); ?>;
-			//]]>
-		</script>
 		<script type="text/javascript" src="lib/standard.js"></script>
 	</head>
 	<body>
