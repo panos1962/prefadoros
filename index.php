fe17c1c844481cc0e5f5b1bce69a5613aec4098b
panos1962 <panos1962@localhost>
2011-06-29 09:55:03 +0300

    modifications

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 			'πρεφαδόρος, ατού, αγορά, μπάζα, μπάζες, σόλο, σολαρία, ' .
 			'κάσα, καπίκι, καπίκια'; ?>" />
 		<title>Πρεφαδόρος</title>
+		<link rel="stylesheet" type="text/css" href="lib/standard.css" />
 		<script type="text/javascript">
 			//<![CDATA[
 			globals = {};
@@ -19,6 +20,14 @@
 		<script type="text/javascript" src="lib/standard.js"></script>
 	</head>
 	<body>
+		<div class="testBoxArea">
+			<span onclick="testAjax(1)" class="testBox" >Test1</span>
+		</div>
+		<div class="testBoxArea">
+			<span onclick="testAjax(2)" class="testBox" >Test2</span>
+		</div>
 		Ok!
+		<div id="info">
+		</div>
 	</body>
 </html>
