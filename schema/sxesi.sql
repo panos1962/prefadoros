e635d1248ed1a4c15d6a250b4021e5fb70a8e74d
panos1962 <panos1962@localhost>
2011-07-01 17:33:18 +0300

    modifications

---
+++ schema/sxesi.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `σχέση` (
+	`κωδικός`	INTEGER(10) AUTO_INCREMENT NOT NULL COMMENT 'Primary key',
+	`παίκτης`	CHARACTER(32) NOT NULL COMMENT 'Login name του παίκτη',
+	`σχετιζόμενος`	CHARACTER(32) NOT NULL COMMENT 'Login name του σχετιζόμενου παίκτη',
+	`status`	ENUM (
+		'ΦΙΛΟΣ',
+		'ΑΠΟΚΛΕΙΣΜΕΝΟΣ'
+	)		NOT NULL COMMENT 'Είδος σχέσης',
+	`δημιουργία`	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Ημερομηνία δημιουργίας',
+
+	PRIMARY KEY (
+		`κωδικός`
+	) USING HASH,
+
+	FOREIGN KEY (
+		`παίκτης`
+	) REFERENCES `παίκτης` (
+		`login`
+	) ON DELETE CASCADE,
+
+	FOREIGN KEY (
+		`σχετιζόμενος`
+	) REFERENCES `παίκτης` (
+		`login`
+	) ON DELETE CASCADE,
+
+	UNIQUE KEY (
+		`παίκτης`,
+		`σχετιζόμενος`
+	) USING BTREE
+) ENGINE=InnoDB CHARSET=utf8 COMMENT='Πίνακας σχέσεων'
