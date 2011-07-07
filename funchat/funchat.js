c5fd54f25d91d582bd0a97376d31e0395d32d626
panos1962 <panos1962@localhost>
2011-07-07 23:32:08 +0300

    modifications

--- funchat/funchat.js
+++ funchat/funchat.js
@@ -1,25 +1,5 @@
 var funchat = {};
 
-funchat.resize = function() {
-	var x = window.opener;
-	if (notSet(x) || notSet(x.globals)) {
-		return;
-	}
-
-	x.globals.funchatWhlt = window.innerWidth + ':' + window.innerHeight + ':' +
-		window.screenLeft + ':' + window.screenTop;
-
-	if (isSet(funchat.whltSaver)) {
-		clearTimeout(funchat.whltSaver);
-	}
-	funchat.whltSaver = setTimeout(function() {
-		if (isSet(x.globals.funchatWhlt)) {
-			var req = new Request('funchat/whltSave');
-			req.send('whlt=' + x.globals.funchatWhlt);
-		}
-	}, 1000);
-};
-
 window.onload = function() {
 	init();
 };
