ef0ea5e6245c73e10c74fef2d4774c9b3d34a8d2
panos1962 <panos1962@localhost>
2011-07-08 19:31:28 +0300

    modifications

--- funchat/funchat.js
+++ funchat/funchat.js
@@ -1,4 +1,70 @@
-var funchat = {};
+funchat = {};
+
+funchat.whltSaveTimer = null;
+
+funchat.whltSave = function() {
+	var x = window.opener;
+	if (notSet(x)) {
+		return;
+	}
+
+	if (isSet(window.screenLeft)) {
+		var l = window.screenLeft;
+	}
+	else if (isSet(window.screenX)) {
+		l = window.screenX;
+	}
+	else {
+		return;
+	}
+
+	if (isSet(window.screenTop)) {
+		var t = window.screenTop;
+	}
+	else if (isSet(window.screenY)) {
+		t = window.screenY;
+	}
+	else {
+		return;
+	}
+
+	if (isSet(window.outerWidth)) {
+		var w = window.outerWidth;
+	}
+	else if (isSet(document) && isSet(document.body) && isSet(document.body.clientWidth)) {
+		w = document.body.clientWidth;
+	}
+	else {
+		return;
+	}
+
+	if (isSet(window.outerHeight)) {
+		var h = window.outerHeight;
+	}
+	else if (isSet(screen) && isSet(screen.availHeight)) {
+		h = screen.availHeight;
+	}
+	else {
+		return;
+	}
+
+	if (isSet(funchat.whltSaveTimer)) {
+		clearTimeout(funchat.whltSaveTimer);
+	}
+
+	funchat.whltSaveTimer = setTimeout(function() {
+		x.globals.funchatWhlt = w + ':' + h + ':' + l + ':' + t;
+		var req = new Request('funchat/whltSave', false);
+		req.send('whlt=' + x.globals.funchatWhlt);
+		var rsp = req.getResponse();
+		if (rsp) {
+			x.mainFyi('funcaht/whltSave.php: ' + rsp);
+		}
+		else {
+			x.mainFyi('saved funcaht panel position &amp; size', 500);
+		}
+	}, 5000);
+}
 
 window.onload = function() {
 	init();
