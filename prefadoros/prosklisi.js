522e0af707c4295352e870aeda4e3ba485f98dc9
panos1962 <panos1962@localhost>
2011-07-26 22:37:58 +0300

    modifications

--- prefadoros/prosklisi.js
+++ prefadoros/prosklisi.js
@@ -30,6 +30,7 @@ var Prosklisi = new function() {
 		// Αν έχει επιστραφεί array "prosklisiNew", τότε πρόκειται για νέες
 		// εγγραφές τις οποίες θα εμφανίσω πρώτες.
 		if (isSet(dedomena.prosklisiNew)) {
+			playSound('sfirigma');
 			for (var i = 0; i < dedomena.prosklisiNew.length; i++) {
 				prosklisi1[prosklisi1.length] = dedomena.prosklisiNew[i];
 			}
@@ -38,6 +39,7 @@ var Prosklisi = new function() {
 		// Διατρέχω το παλιό array "prosklisi" και ελέγχω αν κάποιες από τις
 		// εγγραφές του έχουν διαγραφεί. Τις εγγραφές που εμφανίζονται
 		// να έχουν διαγραφεί τις αγνοώ.
+		if (isSet(dedomena.prosklisiDel)) { playSound('skisimo'); }
 		for (var i = 0; i < prosklisi.length; i++) {
 			if (isSet(dedomena.prosklisiDel) &&
 					(('p_' + prosklisi[i].k) in dedomena.prosklisiDel)) {
@@ -67,17 +69,44 @@ var Prosklisi = new function() {
 	};
 
 	this.HTML = function(i) {
+		var apoMena = (prosklisi[i].p != pektis.login);
 		var html = '';
 		html += '<div class="prosklisiLine zebra' + (i % 2);
+		if (apoMena) { html += ' prosklisiApo'; }
 		html += '" onmouseover="emfanesAfanes(this, true);" ';
 		html += 'onmouseout="emfanesAfanes(this, false);">';
-		html += '<img class="prosklisiIcon" src="' + globals.server + 'images/' +
-			(prosklisi[i].p == pektis.login ? 'prosEmena' : 'apoMena') +
-			'.png" alt="" />'
-html += prosklisi[i].p;
-		html += '<div class="prosklisiData">' + prosklisi[i].k + '</div>';
-		html += '</div>'
+		html += '<div>';
+		if (apoMena) {
+			html += '<img class="prosklisiIcon" src="' + globals.server +
+				'images/apoMena.png" style="cursor: default;" alt="" />';
+			html += '<img class="prosklisiIcon" src="' + globals.server +
+				'images/Xred.png" onclick="Prosklisi.skisimo(' +
+				prosklisi[i].k + ');" title="Ανάκληση πρόσκλησης" alt="" />';
+			html += '<span>προς <div class="prosklisiData">' + prosklisi[i].p + '</div>';
+		}
+		else {
+			var apodoxi = ' title="Αποδοχή πρόσκλησης" onclick="Prosklisi.apodoxi(' +
+				prosklisi[i].k + ');" ';
+			html += '<img class="prosklisiIcon" src="' + globals.server +
+				'images/prosEmena.png"' + apodoxi + 'alt="" />';
+			html += '<img class="prosklisiIcon" src="' + globals.server +
+				'images/Xred.png" onclick="Prosklisi.skisimo(' +
+				prosklisi[i].k + ');" title="Απόρριψη πρόσκλησης" alt="" />';
+			html += '<span' + apodoxi + 'style="cursor: pointer;">' +
+				'από <div class="prosklisiData">' + prosklisi[i].a + '</div>';
+		}
+		html += ', τραπέζι <div class="prosklisiData">' + prosklisi[i].t + '</div>';
+		html += '</span></div>';
+		html += '</div>';
 
 		return html;
+	};
+
+	this.apodoxi = function(k) {
+		mainFyi('αποδοχή πρόσκλησης ' + k);
+	};
+
+	this.skisimo = function(k) {
+		mainFyi('σκίσιμο πρόσκλησης ' + k);
 	};
 };
