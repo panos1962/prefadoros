02d96a70fc0d4d528ce56240be5f577d84f8dcb3
panos1962 <panos1962@localhost>
2011-07-31 14:22:52 +0300

    modifications

--- trapezi/idiotikotita.php
+++ trapezi/idiotikotita.php
@@ -14,11 +14,27 @@ if (!$globals->trapezi->is_pektis()) {
 	die('Δεν έχετε δικαίωμα αλλαγής της ιδιωτικότητας του τραπεζιού');
 }
 
-$idiotikotita = $globals->asfales(Globals::perastike_check('idiotikotita'));
-$query = "UPDATE `τραπέζι` SET `ιδιωτικότητα` = '" . $idiotikotita .
+$idiotikotita = Globals::perastike_check('idiotikotita');
+@mysqli_autocommit($globals->db, FALSE);
+
+$query = "UPDATE `τραπέζι` SET `ιδιωτικότητα` = '" . $globals->asfales($idiotikotita) .
 	"' WHERE `κωδικός` = " . $globals->trapezi->kodikos;
 $globals->sql_query($query);
 if (mysqli_affected_rows($globals->db) != 1) {
+	@mysqli_rollback($globals->db);
 	die('Δεν άλλαξε η ιδιωτικότητα του τραπεζιού');
 }
+
+if ($idiotikotita != 'ΔΗΜΟΣΙΟ') {
+	$query = "DELETE FROM `θεατής` WHERE (`τραπέζι` = " . $globals->trapezi->kodikos .
+		") AND (`παίκτης` NOT IN (SELECT `ποιον` FROM `πρόσκληση` WHERE `τραπέζι` = " .
+		$globals->trapezi->kodikos . "))";
+	$result = @mysqli_query($globals->db, $query);
+	if (!$result) {
+		@mysqli_rollback($globals->db);
+		die('Απέτυχε η αποπομπή των απρόσκλητων θεατών');
+	}
+}
+
+@mysqli_commit($globals->db);
 ?>
