dfdc60452c7b11d561ee03a5a5ce7382f8f450d8
panos1962 <panos1962@localhost>
2011-08-08 12:43:43 +0300

    modifications

--- sizitisi/writing.php
+++ sizitisi/writing.php
@@ -8,7 +8,6 @@ Page::data();
 set_globals();
 
 Prefadoros::pektis_check();
-Sizitisi::cleanup_writing();
 switch ($pk = Globals::perastike_check('pk')) {
 case 'P':
 	$trapezi = vres_trapezi();
@@ -19,9 +18,22 @@ case 'K':
 	$sxolio = "@WK@";
 	break;
 default:
+	Sizitisi::cleanup_writing();
 	die(0);
 }
 
+// Πρώτα δοκιμάζουμε να ενημερώσουμε τυχόν υπάρχοντα writing
+// σχόλια για τον παίκτη.
+$prosfata = time() - WRITING_CLEANUP;
+$query = "UPDATE `συζήτηση` SET `σχόλιο` = '" . $globals->asfales($sxolio) .
+ 	"', `τραπέζι` = " . $trapezi . " WHERE (`παίκτης` LIKE " .
+	$globals->pektis->slogin . ") AND (`σχόλιο` REGEXP '^@W[PK]@$') " .
+	"AND (UNIX_TIMESTAMP(`πότε`) > " . $prosfata . ")";
+@mysqli_query($globals->db, $query);
+if (@mysqli_affected_rows($globals->db) > 0) { die(0); }
+
+// Εφόσον δεν ενημερώθηκαν υπάρχοντα writing σχόλια για τον
+// παίκτη, εισάγουμε νέο writing σχόλιο.
 $query = "INSERT INTO `συζήτηση` (`παίκτης`, `τραπέζι`, `σχόλιο`) " .
 	"VALUES (" . $globals->pektis->slogin . ", " . $trapezi . ", '" .
 	$globals->asfales($sxolio) . "')";
