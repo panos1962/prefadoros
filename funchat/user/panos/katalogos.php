36ccaf2d917ccc9fb00f7a519f1eb48b0aa98fbc
panos1962 <panos1962@localhost>
2011-10-05 20:03:21 +0300

    modifications

--- funchat/user/panos/katalogos.php
+++ funchat/user/panos/katalogos.php
@@ -1,4 +1,5 @@
 <?php
-$item_list[] = new Item("user/panos/klarino.gif");
+$item_list[] = new Item("user/panos/klarino.gif",
+	"Να κεράσουμε λίγο κορμό σοκολάτα;");
 $item_list[] = new Item("user/panos/oboe.gif", "", 6.0);
 ?>
