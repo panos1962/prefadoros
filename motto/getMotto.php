c23130d0498b383f021c11ee7358604278e32fc9
panos1962 <panos1962@localhost>
2011-10-24 03:45:15 +0300

    modifications

--- motto/getMotto.php
+++ motto/getMotto.php
@@ -4,7 +4,7 @@ require_once '../prefadoros/prefadoros.php';
 Page::data();
 set_globals();
 
-if (!$fsize = filesize($fname = "../motto/motto.txt")) {
+if (!($fsize = filesize($fname = "../motto/motto.txt"))) {
 	die(0);
 }
 
@@ -12,10 +12,16 @@ $fh = fopen($fname, "r");
 if (!$fh) {
 	die(0);
 }
-fread($fh, mt_rand(0, $fsize));
 
 $text = "";
 $author = "";
+
+$tixeo = mt_rand(0, $fsize);
+if (fseek($fh, $tixeo) != 0) {
+	rewind($fh);
+	fread($fh, $tixeo);
+}
+
 $mode = 0;
 while ($line = fgets($fh)) {
 	if (preg_match("/^#[\r\n]/", $line)) {
