66b711d32fd806e4186a2afdcc6c527ff554dd24
panos1962 <panos1962@localhost>
2011-12-10 16:44:51 +0200

    modifications

--- prefadoros/sizitisi.js
+++ prefadoros/sizitisi.js
@@ -290,7 +290,7 @@ var Sizitisi = new function() {
 		//
 		// είναι η εικόνα μας αρκετά στενότερη.
 
-		if (s.match(/^https?:\/\/.*\.(jpe?g|png|gif)[-+=]*$/i)) {
+		if (s.match(/^https?:\/\/.*\.(jpe?g|png|gif|bmp)[-+=]*$/i)) {
 			var url = s.replace(/[-+=]*$/, '');
 			var w = dfltw;
 			if (url != s) {
