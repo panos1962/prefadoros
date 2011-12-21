0dba71d55297ab6ee6fecbc935e7b0861e318dff
panos1962 <panos1962@localhost>
2011-12-21 11:23:44 +0200

    modifications

--- account/loginCheck.php
+++ account/loginCheck.php
@@ -7,8 +7,8 @@ set_globals();
 
 Globals::perastike_check('login');
 Globals::perastike_check('password');
-$query = "SELECT `paraskinio` FROM `pektis` WHERE `login` = '" .
-	$globals->asfales($_REQUEST['login']) . "' AND `password` = '" .
+$query = "SELECT `paraskinio` FROM `pektis` WHERE `login` COLLATE utf8_bin = '" .
+	$globals->asfales($_REQUEST['login']) . "' AND `password` COLLATE utf8_bin = '" .
 	$globals->asfales(sha1($_REQUEST['password'])) . "'";
 $result = $globals->sql_query($query);
 $row = @mysqli_fetch_array($result, MYSQLI_NUM);
