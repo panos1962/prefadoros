6c989096c2006c6b4bbbb415b9c7c616d861c9a9
panos1962 <panos1962@localhost>
2011-12-27 01:18:30 +0200

    modifications

--- prefadoros/partida.js
+++ prefadoros/partida.js
@@ -229,7 +229,7 @@ var Partida = new function() {
 			songs = [ 'TeNJmOsBS94', 'DW5bIN4h6O4', 'sbbQwecCzo8' ];
 			break;
 		case 'neoEtos.png':
-			songs = [ '_6xNuUEnh2g', 'EmKlGcAK8DE' ];
+			songs = [ '_6xNuUEnh2g', 'EmKlGcAK8DE', 'Nho5Zl_IrrE' ];
 			break;
 		}
 
