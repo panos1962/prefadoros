10e004d3ef985e72868d23e0fceeeaaf23fc5360
panos1962 <panos1962@localhost>
2012-01-03 16:22:44 +0200

    modifications

--- funchat/user/lalaridi/katalogos.php
+++ funchat/user/lalaridi/katalogos.php
@@ -1,8 +1,4 @@
 <?php
-$item_list[] = new Item("user/panos/klarino.gif",
-	"Να κεράσουμε λίγο κορμό σοκολάτα;");
-$item_list[] = new Item("user/panos/oboe.gif",
-	"Να τρατάρουμε ένα «υποβρύχιο»;", 6.0);
 $item_list[] = new Item("user/panos/staExigoOrea.jpg",
 	"Βρε, στα εξηγώ ωραία;", 4.0, "http://splicd.com/oOLJc2hRtVw/36/45");
 $item_list[] = new Item("user/panos/omorfantraMou.jpg",
