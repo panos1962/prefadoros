71b52ee42d12d98e31df1b3a903158da7960b42b
panos1962 <panos1962@localhost>
2012-01-04 03:52:46 +0200

    modifications

--- funchat/user/panos/katalogos.php
+++ funchat/user/panos/katalogos.php
@@ -9,4 +9,6 @@ $item_list[] = new Item("user/panos/omorfantraMou.jpg",
 	"Ομορφάντρα μουυυ!", 4.0, "http://splicd.com/kX76iMTqzNo/40/44");
 $item_list[] = new Item("user/panos/kritikos.jpg",
 	"Η πρέφα θέλει υπομονή!", 4.0, "http://splicd.com/fYGBzNScM0I/24/28");
+$item_list[] = new Item("user/panos/orePouPame.jpg",
+	"Ωρέ πού πάμε, ρε; Πού πάμε;", 6.0, "http://youtu.be/Tr1JPeAW3M8?start=6");
 ?>
