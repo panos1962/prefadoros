c101b013d631e9e85f996058c457842a8e2bac1f
panos1962 <panos1962@localhost>
2012-01-10 03:52:53 +0200

    modifications

--- movie/index.php
+++ movie/index.php
@@ -8,21 +8,34 @@ Page::javascript('movie/movie');
 Page::body();
 ?>
 <div id="main" class="movieMain">
-<div id="epikefalida" class="movieEpikefalida">
-<table width="100%" class="tbldbg">
-<tr>
-<td class="movieEpikefalidaLeft tbldbg">
-Παρτίδα <?php print $trapezi; ?>, κάσα 50
-</td>
-<td class="movieEpikefalidaRight tbldbg">
-Διανομή
-</td>
-</tr>
-</table>
-</div>
-<div id="trapezi" class="movieTrapezi">
-asjgdhasj
-</div>
+	<div id="epikefalida" class="movieEpikefalida">
+		<table width="100%" class="tbldbg">
+		<tr>
+		<td class="movieEpikefalidaLeft tbldbg">
+		Παρτίδα <?php print $trapezi; ?>, κάσα 50
+		</td>
+		<td class="movieEpikefalidaRight tbldbg">
+		Διανομή
+		</td>
+		</tr>
+		</table>
+	</div>
+	<div id="trapezi" class="movieTrapezi">
+		<div style="position: relative; height: 18.0cm;">
+			<div class="moviePektisArea moviePektisArea3">
+				3
+			</div>
+			<div class="moviePektisArea moviePektisArea2">
+				2
+			</div>
+			<div class="moviePektisArea moviePektisArea1">
+				1
+			</div>
+		</div>
+	</div>
+	<div class="movieControls">
+		Controls
+	</div>
 </div>
 <?php
 Page::close(FALSE);
