980cc9ef7d613f748e470b742a2618a99fc89295
panos1962 <panos1962@localhost>
2012-01-30 15:06:39 +0200

    modifications

--- funchat/user/panos/katalogos.php
+++ funchat/user/panos/katalogos.php
@@ -3,6 +3,8 @@ $item_list[] = new Item("user/panos/klarino.gif",
 	"Να κεράσουμε λίγο κορμό σοκολάτα;");
 $item_list[] = new Item("user/panos/oboe.gif",
 	"Να τρατάρουμε ένα «υποβρύχιο»;", 6.0);
+$item_list[] = new Item("user/panos/axouAxou.png",
+	"Άχου, άχου στο σταθμό Μονάχου", 6.0);
 $item_list[] = new Item("user/panos/staExigoOrea.jpg",
 	"Βρε, στα εξηγώ ωραία;", 4.0, "http://splicd.com/oOLJc2hRtVw/36/45");
 $item_list[] = new Item("user/panos/omorfantraMou.jpg",
