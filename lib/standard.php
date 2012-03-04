1dee511fee25211fe2ac477be33c860f6ff7db16
panos1962 <panos1962@localhost>
2012-03-04 02:12:57 +0200

    modifications

--- lib/standard.php
+++ lib/standard.php
@@ -190,7 +190,7 @@ class Globals {
 	public static function get_line($fh) {
 		$line = fgets($fh);
 		if ($line) {
-			return(rtrim($line));
+			return(preg_replace("/[\r\n].*/", '', $line));
 		}
 		return(FALSE);
 	}
