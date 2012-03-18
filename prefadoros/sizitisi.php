833c466daffaa6a9aa2041077cc2a43678ebe1e3
panos1962 <panos1962@localhost>
2012-03-18 21:41:27 +0200

    modifications

--- prefadoros/sizitisi.php
+++ prefadoros/sizitisi.php
@@ -472,19 +472,17 @@ class Sizitisi {
 	public static function set_dirty($ola = TRUE, $trapezi = NULL) {
 		global $globals;
 
-		$query = "UPDATE `sinedria` SET `sizitisidirty` = 2 WHERE (`sizitisidirty` <> 2)";
+		//$query = "UPDATE `sinedria` SET `sizitisidirty` = 2 WHERE (`sizitisidirty` <> 2)";
+		$query = "UPDATE `sinedria` SET `sizitisidirty` = 2";
 
+	/*
 		if (!$ola) {
 			$query .= " AND (`pektis` != BINARY " . $globals->pektis->slogin . ")";
 		}
+	*/
 
 		if (isset($trapezi)) {
-			if ($trapezi == "NULL") {
-				$query .= " AND (`trapezi` = 0)";
-			}
-			else {
-				$query .= " AND (`trapezi` = " . $trapezi . ")";
-			}
+			$query .= " WHERE (`trapezi` = " . ($trapezi == "NULL" ? 0 : $trapezi);
 		}
 
 		@mysqli_query($globals->db, $query);
