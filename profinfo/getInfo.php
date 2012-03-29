4264e2f904b537d23663abd71a4256cefe3888b2
panos1962 <panos1962@localhost>
2012-03-29 22:53:59 +0300

    modifications

--- profinfo/getInfo.php
+++ profinfo/getInfo.php
@@ -8,6 +8,25 @@ Prefadoros::pektis_check();
 $pektis = Globals::perastike_check("pektis");
 $apektis = "'" . $globals->asfales($pektis) . "'";
 
+$onoma = "";
+$query = "SELECT `onoma` FROM `pektis` WHERE `login` = BINARY " . $apektis;
+$result = $globals->sql_query($query);
+$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+if ($row) {
+	$onoma = $row[0];
+}
+@mysqli_free_result($result);
+
+$status = "";
+$query = "SELECT `status` FROM `sxesi` WHERE (`pektis` = BINARY " .
+	$globals->pektis->slogin . ") AND (`sxetizomenos` = BINARY " . $apektis . ")";
+$result = $globals->sql_query($query);
+$row = @mysqli_fetch_array($result, MYSQLI_NUM);
+if ($row) {
+	$status = $row[0];
+}
+@mysqli_free_result($result);
+
 $mine = "";
 $query = "SELECT `kimeno` FROM `profinfo` WHERE (`sxoliastis` = BINARY " .
 	$globals->pektis->slogin . ") AND (`pektis` = BINARY " . $apektis . ")";
@@ -30,5 +49,5 @@ if ($globals->pektis->login != $pektis) {
 	@mysqli_free_result($result);
 }
 
-$globals->klise_fige($mine . "\t" . $enim . "\t");
+$globals->klise_fige($onoma . "\t" . $status . "\t" . $mine . "\t" . $enim . "\t");
 ?>
