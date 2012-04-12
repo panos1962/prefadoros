5b477b094e32e71194450186daa07df42248a172
panos1962 <panos1962@localhost>
2012-04-12 14:26:53 +0300

    modifications

--- account/loginAvailable.php
+++ account/loginAvailable.php
@@ -4,12 +4,13 @@ Page::data();
 set_globals();
 
 Globals::perastike_check('login');
-$query = "SELECT * FROM `pektis` WHERE `login` = BINARY '" .
+$query = "SELECT * FROM `pektis` WHERE `login` = '" .
 	$globals->asfales($_REQUEST['login']) . "'";
-$result = $globals->sql_query($query, 'account/loginAvaileable.php');
+$result = $globals->sql_query($query, 'account/loginAvailable.php');
 $row = @mysqli_fetch_array($result);
 if ($row) {
 	@mysqli_free_result($result);
-	die('Το όνομα "' . $_REQUEST['login'] . '" δεν είναι διαθέσιμο');
+	die('Το όνομα <span class="errorFyiData">' . $_REQUEST['login'] .
+		'</span> δεν είναι διαθέσιμο');
 }
 ?>
