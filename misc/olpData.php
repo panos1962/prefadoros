938c86804f5b8606d260d0626804c3bf9f33ef9e
panos1962 <panos1962@localhost>
2012-05-08 10:08:19 +0300

    modifications

--- misc/olpData.php
+++ misc/olpData.php
@@ -10,6 +10,8 @@ require_once '../prefadoros/prefadoros.php';
 set_globals();
 Prefadoros::set_pektis();
 
+global $trapezi;
+
 $tcount = trapezi_count();
 $energos = energos_pektis();
 $sxesi = sxesi();
@@ -61,12 +63,16 @@ class OLP {
 	}
 
 	public function json($sxesi = NULL) {
+		global $trapezi;
 		static $koma = "";
 
 		print $koma . "{l:'" . $this->login . "',o:'" .
 			Globals::asfales_json($this->onoma) . "'";
 		if (isset($sxesi)) {
 			print ",s:'" . $sxesi . "'";
+		}
+		if (array_key_exists($this->login, $trapezi)) {
+			print ",t:" . (int)$trapezi[$this->login];
 		}
 		if ($this->katastasi == 'BUSY') {
 			print ",b:1";
@@ -78,15 +84,20 @@ class OLP {
 
 function trapezi_count() {
 	global $globals;
+	global $trapezi;
 
+	$trapezi = array();
 	$tcount = 0;
 	$tora_ts = time() - XRONOS_PEKTIS_IDLE_MAX;
-	$query = "SELECT COUNT(*) FROM `trapezi` " .
+	$query = "SELECT `kodikos`, `pektis1`, `pektis2`, `pektis3` FROM `trapezi` " .
 		"WHERE UNIX_TIMESTAMP(`poll`) > " .
 		$tora_ts;
 	$result = $globals->sql_query($query);
 	while ($row = @mysqli_fetch_array($result, MYSQLI_NUM)) {
-		$tcount = $row[0];
+		$tcount++;
+		$trapezi[$row[1]] = $row[0];
+		$trapezi[$row[2]] = $row[0];
+		$trapezi[$row[3]] = $row[0];
 	}
 
 	return($tcount);
