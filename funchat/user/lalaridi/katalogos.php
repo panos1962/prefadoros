81363904c121d8620d7467d2d86647ff79ca7f42
panos1962 <panos1962@localhost>
2012-07-09 11:02:14 +0300

    modifications

--- funchat/user/lalaridi/katalogos.php
+++
@@ -1,8 +0,0 @@
-<?php
-$item_list[] = new Item("user/panos/staExigoOrea.jpg",
-	"Βρε, στα εξηγώ ωραία;", 4.0, "http://youtu.be/H9VKYRfvp3s");
-$item_list[] = new Item("user/panos/omorfantraMou.jpg",
-	"Ομορφάντρα μουυυ!", 4.0, "http://splicd.com/kX76iMTqzNo/40/44");
-$item_list[] = new Item("user/panos/kritikos.jpg",
-	"Η πρέφα θέλει υπομονή!", 4.0, "http://splicd.com/fYGBzNScM0I/24/28");
-?>
