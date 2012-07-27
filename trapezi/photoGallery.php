3451165e3bc9192bb9980981c7b88ed5089825ad
panos1962 <panos1962@localhost>
2012-07-27 12:26:41 +0300

    modifications

--- trapezi/photoGallery.php
+++ trapezi/photoGallery.php
@@ -1,9 +1,18 @@
 <?php
+/*
+Πρόκειται για πρόγραμμα το οποίο καλείται μέσω AJAX call και επιστρέφει
+τα ονόματα των αρχείων εικόνας που βρίσκονται στο directory "gallery"
+κάτω από το directory "images". Οι εικόνες αυτές προβάλλονται στο χώρο
+του καφενείου όταν δεν υπάρχουν τραπέζια και οι περιφερόμενοι παίκτες
+είναι λιγοστοί, οπότε υπάρχει αρκετός διαθέσιμος χώρος. Τα ονόματα
+των αρχείων εικόνας επιστρέφονται ως comma separated list από strings.
+*/
+
 require_once '../lib/standard.php';
 Page::data();
 set_globals(TRUE);
 
-$list = scandir("../images/gallery");
+$list = @scandir("../images/gallery");
 if ($list === FALSE) { die(0); }
 if (($n = count($list)) < 1) { die(0); }
 
