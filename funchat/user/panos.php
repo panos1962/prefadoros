d99b92cbbb6bfc7972449528d1273318b9a296c9
panos1962 <panos1962@localhost>
2012-07-30 19:46:14 +0300

    modifications

--- funchat/user/panos.php
+++ funchat/user/panos.php
@@ -1,23 +1,20 @@
 <?php
-$item_list[] = new Item("osamaBushSmall.gif",
-	"Πάμε μαζί, με τρέλα και κορδέλα;", 4.6);
-$item_list[] = new Item("user/panos/axouAxou.png",
-	"", 6.0);
-$item_list[] = new Item("user/panos/staExigoOrea.jpg",
-	"Βρε, στα εξηγώ ωραία;", 4.0, "http://youtu.be/H9VKYRfvp3s");
-$item_list[] = new Item("user/panos/omorfantraMou.jpg",
-	"Ομορφάντρα μουυυ!", 4.0, "http://youtu.be/1vKYJ2nm1W0");
-$item_list[] = new Item("user/panos/kritikos.jpg",
-	"Η πρέφα θέλει υπομονή!", 4.0);
+$item_list[] = new Item("osamaBushSmall.gif", "Πάμε μαζί, με τρέλα και κορδέλα;", 4.6);
+$item_list[] = new Item("user/panos/axouAxou.png", "", 6.0);
+$item_list[] = new Item("user/panos/staExigoOrea.jpg", "Βρε, στα εξηγώ ωραία;", 4.0,
+	"http://youtu.be/H9VKYRfvp3s");
+$item_list[] = new Item("user/panos/omorfantraMou.jpg", "Ομορφάντρα μουυυ!", 4.0,
+	"http://youtu.be/1vKYJ2nm1W0");
+$item_list[] = new Item("user/panos/kritikos.jpg", "Η πρέφα θέλει υπομονή!", 4.0);
 $item_list[] = new Item("user/panos/marinikol.gif", "", 4.0);
-$item_list[] = new Item("", "Ωρέ πού πάμε, ρε; Πού πάμε;", 6.0,
+$item_list[] = new Item("user/panos/orePouPame.jpg", "Ωρέ πού πάμε, ρε; Πού πάμε;", 6.0,
 	"http://youtu.be/U6mfw_0bD1w");
-$item_list[] = new Item("", "Πώ, ρε, τι θα γίνω εγώ με σένα…", 6.6, "_KG_");
-$item_list[] = new Item("", "Βρε δεν ντρέπεσαι, γέρος άνθρωπος βρε;", 6.0,
+$item_list[] = new Item("kitrinaGantia.jpg", "Πώ, ρε, τι θα γίνω εγώ με σένα…", 6.6, "_KG_");
+$item_list[] = new Item("user/panos/denDrepese.jpg", "Βρε δεν ντρέπεσαι, γέρος άνθρωπος βρε;", 6.0,
 	"http://youtu.be/Daggdx-enM4");
 $item_list[] = new Item("spiderman.gif", "", "3.0", "_NC_");
 $item_list[] = new Item("soldierDance.gif", "", "", "_BG_");
 $item_list[] = new Item("soloDance.gif", "", "", "_ITM_");
-$item_list[] = new Item("", "Αργούμε;", "3.0", "treno");
-$item_list[] = new Item("", "Τι περιμένουμε;", "2.0", "clocktickslow");
+$item_list[] = new Item("treno.gif", "", "3.0", "treno");
+$item_list[] = new Item("argoporia.gif", "Τι περιμένουμε;", "2.0", "clocktickslow");
 ?>
