f32aaea5dd376e45cf07c0220d03a8066cbbf1e0
panos1962 <panos1962@localhost>
2012-08-28 19:40:21 +0300

    modifications

--- prefadoros/setOptiki.php
+++ prefadoros/setOptiki.php
@@ -9,7 +9,4 @@ $trapezi = Globals::perastike_check('trapezi');
 $query = "UPDATE `sinedria` SET `trapezi` = " . $trapezi .
 	", `sizitisidirty` = 2, `trapezidirty` = 2 WHERE `kodikos` = " . $sinedria;
 @mysqli_query($globals->db, $query);
-if (@mysqli_affected_rows($globals->db) != 1) {
-	print "Δεν ενημερώθηκε άποψη συνεδρίας";
-}
 $globals->klise_fige();
